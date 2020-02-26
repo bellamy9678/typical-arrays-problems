0154fcb0d398c4b41253cd7a6897573f5b3e2374
Michael Fursa <bellamy9678@yahoo.com>
2020-02-26 15:19:59 +0300

    feat: typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  let elem = 0;
+
+  if (arguments.length != 1 || array.length == 0) {
+    return elem;
+  } else {
+    elem = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < elem) {
+        elem = array[i];
+      };
+    }
+  }
+  return elem;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let elem = 0;
+
+  if (arguments.length != 1 || array.length == 0) {
+    return elem;
+  } else {
+    elem = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > elem) {
+        elem = array[i];
+      };
+    }
+  }
+  return elem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let elem = 0;
+
+  if (arguments.length != 1 || array.length == 0) {
+    return elem;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      elem += array[i];
+    }
+  }
+  return elem/array.length;
 }
